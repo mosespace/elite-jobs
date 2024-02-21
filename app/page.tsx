45ad38012c1763652ad899ab8e0d0fed573a4ddf
Uncle Moses <111237257+mosespace@users.noreply.github.com>
2024-02-21 10:50:40 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className='flex min-h-screen items-center justify-center'>
+      Hello World
+    </main>
+  )
+}
