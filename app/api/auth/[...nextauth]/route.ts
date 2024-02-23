b7d52b8eb243c1362f19fbefb9b9a2c0f540db0b
mosespace <kisakyemoses50@gmail.com>
2024-02-23 15:36:56 +0300

    feat: initial commit 👋

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,5 +3,5 @@ import NextAuth from 'next-auth'
 
 // Define your handler function
 const handler = NextAuth(authOptions)
-// Export the handler and alias it as GET and POST
+// Export the handler and alias as GET and POST
 export { handler as GET, handler as POST }
