4069f540870d9af6a146f46ab62bb7778c1cf995
mosespace <kisakyemoses50@gmail.com>
2024-02-23 13:35:27 +0300

    It's a good one 👍

--- components/frontend/JobListing.tsx
+++ components/frontend/JobListing.tsx
@@ -304,7 +304,7 @@ function JobListing() {
                 <path
                   fillRule='evenodd'
                   d='M6.293 7.293a1 1 0 011.414 1.414L10 11.414l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z'
-                  clip-rule='evenodd'
+                  clipRule='evenodd'
                 />
               </svg>
             </div>
