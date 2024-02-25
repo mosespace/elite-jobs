6713a1d060842595d8f4ed8163cfec5e07259ba0
mosespace <kisakyemoses50@gmail.com>
2024-02-25 10:46:40 +0300

    feat: update user-auth-form.tsx and schema.prisma

--- components/frontend/user-auth-form.tsx
+++ components/frontend/user-auth-form.tsx
@@ -65,7 +65,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
             </Label>
             <Input
               id='email'
-              placeholder='elitejobs@example.com'
+              placeholder='elitejobs@examples.com'
               type='email'
               autoCapitalize='none'
               autoComplete='email'
